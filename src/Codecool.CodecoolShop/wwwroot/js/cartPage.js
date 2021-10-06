8f828f096abd2e0fdb8b548b55a09a530e5c75b0
mihaibuga <77620547+mihaibuga@users.noreply.github.com>
2021-10-06 19:07:49 +0300

    move emptyCartFormat method from cartPage js to htmlFactory

--- src/Codecool.CodecoolShop/wwwroot/js/cartPage.js
+++ src/Codecool.CodecoolShop/wwwroot/js/cartPage.js
@@ -3,43 +3,6 @@
 let shoppingCartPageContainer = document.querySelector(".shopping-cart-page-container");
 let formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', });
 
-//function formatShoppingCartPageItem(item) {
-//    return `
-//        <div class="filled-cart-item">
-//            <div class="filled-cart-item-left">
-//                <img src="/img/${item.Product.Name}.jpg" alt="${item.Product.Name}" />
-//            </div>
-//            <div class="filled-cart-item-right">
-//                <a class="filled-cart-item-name"
-//                    href="" title="${item.Product.Name}">${item.Product.Name}
-//                </a>
-
-//                <div class="filled-cart-item-quantity">
-//                    <button name="decrease-quantity" type="submit"><i class="arrow down"></i></button>
-//                    <p><strong>${item.Quantity} ${item.Quantity == 1 ? "Piece" : "Pieces"}</strong></p>
-//                    <button name="increase-quantity" type="submit"><i class="arrow up"></i></button>
-//                </div>
-
-//                <button class="filled-cart-item-delete" data-product-id="${item.Product.Id}"><i class="fa fa-trash-o"></i></button>
-
-//                <span class="filled-cart-item-price">${formatter.format(item.Product.DefaultPrice * item.Quantity)}</span>
-//            </div>
-//        </div>`;
-//}
-
-function emptyCartFormat() {
-    return `
-        <div id="empty-cart">
-            <div class="shopping-cart-title">Your cart is empty</div>
-            <div class="textBox mrg-btm-sm">
-                <div>To add products to cart</div>
-                <div>please go back to the store.</div>
-            </div>
-            <a class="empty-cart-primary-btn pho-btn btn btn-primary" href="/">
-                <i class="em em-go-left"></i>Back to Store
-            </a>
-        </div>`;
-}
 
 function filledCartFormat() {
     return `
@@ -82,7 +45,9 @@ function loadShoppingCartPage() {
         loadShoppingCartPageContainer(items);
     }
     else {
-        shoppingCartPageContainer.innerHTML = emptyCartFormat();
+        const emptyCartFormatBuilder = htmlFactory(htmlTemplates.emptyCartFormat);
+        const emptyCartFormat = emptyCartFormatBuilder();
+        shoppingCartPageContainer.innerHTML = emptyCartFormat;
     }
 }
 
