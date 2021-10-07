66ffe61d65c7f7baad6f0e4416d860abe18d50df
mihaibuga <77620547+mihaibuga@users.noreply.github.com>
2021-10-07 11:48:03 +0300

    create js file for checkout page and replace cart quantity total in html object

---
+++ src/Codecool.CodecoolShop/wwwroot/js/checkoutPage.js
@@ -0,0 +1,11 @@
+﻿import { htmlFactory, htmlTemplates } from "/js/htmlFactory.js";
+
+let shoppingCartPageContainer = document.querySelector(".shopping-cart-page-container");
+let formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', });
+
+
+let cartTotalQuantityBadgeContainer = document.querySelector(".badge-pill");
+
+let shoppingCartQuantity = sessionStorage.getItem("shoppingCartQuantity");
+
+cartTotalQuantityBadgeContainer.innerHTML = shoppingCartQuantity;
