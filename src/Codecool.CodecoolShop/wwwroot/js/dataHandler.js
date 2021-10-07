817998047ae93b68ff76fb4181afc7f35736be5d
Nicolae Peptea <nicolae.peptea@gmail.com>
2021-10-07 10:08:48 +0300

    create dataHandler.js

---
+++ src/Codecool.CodecoolShop/wwwroot/js/dataHandler.js
@@ -0,0 +1,68 @@
+﻿export let dataHandler = {
+    addNewItemToCart: async function (itemId) {
+
+        let url = "api/add-cart-item";
+        let data = {
+            'id': itemId,
+        };
+        return await apiPost(url, data);
+    },
+    removeItemFromCart: async function (itemId) {
+        let url = "api/remove-cart-item";
+        let data = {
+            'id': itemId,
+        };
+        return await apiDelete(url, data);
+    },
+}
+
+
+async function apiPost(url, data) {
+    try {
+        let response = await $.ajax({
+            method: "post",
+            url: url,
+            data: data,
+            dataType: "json",
+        })
+
+        return response;
+
+    } catch (e) {
+        console.log("Error" + e);
+    }
+}
+
+
+async function apiDelete(url, data) {
+    try {
+        let response = await $.ajax({
+            url: url,
+            data: data,
+            method: "delete",
+            dataType: "json",
+        })
+
+        return response;
+
+    } catch (e) {
+        console.log("Error" + e);
+    }
+}
+
+
+async function apiPut(url, data) {
+    try {
+        let response = await $.ajax({
+            url: url,
+            data: data,
+            method: "put",
+            dataType: "json",
+        })
+
+        return response;
+
+    } catch (e) {
+        console.log("Error" + e);
+    }
+}
