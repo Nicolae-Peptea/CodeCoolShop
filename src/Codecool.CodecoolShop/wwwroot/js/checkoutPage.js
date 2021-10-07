707617f01bb32ed67baf2ff8e529095b17281f0c
mihaibuga <77620547+mihaibuga@users.noreply.github.com>
2021-10-07 12:23:03 +0300

    display items on checkout page designated area

--- src/Codecool.CodecoolShop/wwwroot/js/checkoutPage.js
+++ src/Codecool.CodecoolShop/wwwroot/js/checkoutPage.js
@@ -1,11 +1,38 @@
 ﻿import { htmlFactory, htmlTemplates } from "/js/htmlFactory.js";
 
-let shoppingCartPageContainer = document.querySelector(".shopping-cart-page-container");
+let checkoutPageContainer = document.querySelector(".checkout-page-container");
 let formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', });
 
 
-let cartTotalQuantityBadgeContainer = document.querySelector(".badge-pill");
+function displayCartQuantityInForm() {
+    let cartTotalQuantityBadgeContainer = document.querySelector(".badge-pill");
+    let shoppingCartQuantity = sessionStorage.getItem("shoppingCartQuantity");
+    cartTotalQuantityBadgeContainer.innerHTML = shoppingCartQuantity;
+}
 
-let shoppingCartQuantity = sessionStorage.getItem("shoppingCartQuantity");
 
-cartTotalQuantityBadgeContainer.innerHTML = shoppingCartQuantity;
+function displayItemsFromCart() {
+    let shoppingCartItemsFromSessionStorage = JSON.parse(sessionStorage.getItem("shoppingCartItems"));
+    loadItemsinCheckoutPageItemsContainer(shoppingCartItemsFromSessionStorage);
+}
+
+function loadItemsinCheckoutPageItemsContainer(items) {
+    let checkoutPageItemsContainer = document.querySelector(".list-group.mb-3");
+    let itemsFormat = "";
+    let totalCartSum = 0;
+    const cartItemTemplateBuilder = htmlFactory(htmlTemplates.formatCheckoutPageCartItem);
+    const cartTotalTemplateBuilder = htmlFactory(htmlTemplates.formatCheckoutPageCartTotal);
+
+    for (let i = 0; i < items.length; i++) {
+        const cartItemTemplate = cartItemTemplateBuilder(items[i]);
+
+        itemsFormat += cartItemTemplate;
+        totalCartSum += items[i].Product.DefaultPrice * items[i].Quantity;
+    }
+    const cartTotalTemplate = cartTotalTemplateBuilder(totalCartSum);
+    itemsFormat += cartTotalTemplate;
+    checkoutPageItemsContainer.innerHTML = itemsFormat;
+}
+
+displayCartQuantityInForm();
+displayItemsFromCart();
