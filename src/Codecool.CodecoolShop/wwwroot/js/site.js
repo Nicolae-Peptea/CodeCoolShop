669ffc9f6395290a43d46a71554184cdc73627d6
Nicolae <Nicolae@NICU>
2021-10-15 18:39:28 +0300

    refactor cartUtils

--- src/Codecool.CodecoolShop/wwwroot/js/site.js
+++ src/Codecool.CodecoolShop/wwwroot/js/site.js
@@ -1,37 +1,37 @@
 ﻿import {
-    updateCartItems, showCartQuantityAfterLoading,
-    loadCartItems, initCartButtonFunctionality,
-    AddItemToSessionStorage,
+    initClickEventOnButtons, showCartQuantityAfterLoading,
+    loadCartItems,
 } from "/js/cartUtils.js";
-import { dataHandler } from "/js/dataHandler.js";
 
-
+let shoppingCartPageContainer = document.querySelector(".shopping-cart-page-container");
 let buyButtons = [...document.querySelectorAll("#add-to-cart")];
 let shoppingCart = document.querySelector(".shopping-cart");
 
 
 function initBuyButtons() {
+    const quantity = 1;
     buyButtons.forEach((button) => {
-        button.addEventListener("click", async (event) => {
-            event.preventDefault();
-            console.log(event.target);
-            const htmlElement = event.target;
-            const quantity = 1;
-            AddItemToSessionStorage(htmlElement, quantity);
-            
-
-
-
-            //let functionToHandleUpdate = dataHandler.addNewItemToCart;
-            //await updateCartItems(button, quantity, functionToHandleUpdate)
-
-            if (shoppingCart.style.visibility == "visible") {
-                loadCartItems()
-            }
-        })
+        initClickEventOnButtons(button, quantity);
     })
 }
 
+
+function initCartButtonFunctionality() {
+    let cartButton = document.querySelector("#cart");
+
+    cartButton.addEventListener('click', async function () {
+        event.preventDefault();
+        let visibility = shoppingCart.style.visibility;
+        if (visibility == "visible") {
+            shoppingCart.style.visibility = "hidden";
+            shoppingCartItemsContainer.innerHTML = "";
+        }
+        else {
+            shoppingCart.style.visibility = "visible";
+            loadCartItems();
+        }
+    })
+}
 
 showCartQuantityAfterLoading();
 initCartButtonFunctionality();
