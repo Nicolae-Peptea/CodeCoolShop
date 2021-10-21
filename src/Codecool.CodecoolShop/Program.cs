8ac698a1947f9e4a3e6ea2d7d951fa5900c33b48
Nicolae Peptea <nicolae.peptea@gmail.com>
2021-10-21 15:57:12 +0300

    create serilog confirguration on Main method

--- src/Codecool.CodecoolShop/Program.cs
+++ src/Codecool.CodecoolShop/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Serilog;
 
 namespace Codecool.CodecoolShop
 {
@@ -13,11 +15,32 @@ namespace Codecool.CodecoolShop
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var confirguration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration().
+                ReadFrom.Configuration(confirguration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Codecool Shop starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "The shop failed to start correctly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
