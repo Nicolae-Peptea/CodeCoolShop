7dee9c4de62b532219e3530e99fde2a1ff4402dc
Nicolae Peptea <nicolae.peptea@gmail.com>
2021-11-12 13:47:52 +0200

    create DAO for ProductOrder model from DAL

---
+++ src/Codecool.CodecoolShop/Daos/Implementations/ProductOrderDao.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codecool.CodecoolShop.Daos.Implementations
+{
+    public class ProductOrderDao : IProductOrder
+    {
+        public void Add(ProductOrder item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductOrder Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProductOrder> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
