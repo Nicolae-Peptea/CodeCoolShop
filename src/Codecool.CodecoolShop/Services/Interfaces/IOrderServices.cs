5d718e7b2211d9522a3ebcc95a11073c57ef9d08
mihaibuga <77620547+mihaibuga@users.noreply.github.com>
2021-11-15 12:30:11 +0200

    create interface for OrderServices and assign it's instance where necessary

---
+++ src/Codecool.CodecoolShop/Services/Interfaces/IOrderServices.cs
@@ -0,0 +1,18 @@
+﻿using Codecool.CodecoolShop.Models;
+using DataAccessLayer.Model;
+using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.Services.Interfaces
+{
+    public interface IOrderServices
+    {
+        void Add(int customerId);
+        Order Get(int id);
+        void RemoveItem(int id);
+        IEnumerable<Order> GetAllItems();
+        decimal GetTotalOrderValue(List<ProductOrder> orderItems);
+        List<ProductOrder> UpdateProductOrderPriceFromJson(OrderDetails order);
+        void CreateCustomer(OrderDetails order);
+        void ChargeCustomer(OrderDetails order, decimal orderTotal);
+    }
+}
