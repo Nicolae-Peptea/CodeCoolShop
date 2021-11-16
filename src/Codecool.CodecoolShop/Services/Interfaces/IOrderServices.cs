ead210f8db088d51cf027fac29f380119fc28fee
Nicolae <Nicolae@NICU>
2021-11-16 16:42:10 +0200

    check if the useer is logged

--- src/Codecool.CodecoolShop/Services/Interfaces/IOrderServices.cs
+++ src/Codecool.CodecoolShop/Services/Interfaces/IOrderServices.cs
@@ -1,5 +1,6 @@
 ﻿using Codecool.CodecoolShop.Models;
 using DataAccessLayer.Model;
+using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 
 namespace Codecool.CodecoolShop.Services.Interfaces
@@ -7,12 +8,21 @@ namespace Codecool.CodecoolShop.Services.Interfaces
     public interface IOrderServices
     {
         void Add(int customerId);
+        
         Order Get(int id);
+        
         void RemoveItem(int id);
+        
         IEnumerable<Order> GetAllItems();
+        
         decimal GetTotalOrderValue(List<ProductOrder> orderItems);
+        
         List<ProductOrder> UpdateProductOrderPriceFromJson(OrderDetails order);
+        
         void CreateCustomer(OrderDetails order);
+        
         void ChargeCustomer(OrderDetails order, decimal orderTotal);
+
+        void CreateOrder(OrderDetails order, HttpContext httpContext);
     }
 }
