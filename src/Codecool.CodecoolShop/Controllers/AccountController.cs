00fc69bddc9862fca9b91cbc96d0e65baf6ab9c3
Nicolae <Nicolae@NICU>
2021-11-16 15:05:41 +0200

    delete unused modules

--- src/Codecool.CodecoolShop/Controllers/AccountController.cs
+++ src/Codecool.CodecoolShop/Controllers/AccountController.cs
@@ -1,10 +1,6 @@
-﻿using Codecool.CodecoolShop.Helpers;
-using Codecool.CodecoolShop.ViewModels;
+﻿using Codecool.CodecoolShop.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Codecool.CodecoolShop.Controllers
