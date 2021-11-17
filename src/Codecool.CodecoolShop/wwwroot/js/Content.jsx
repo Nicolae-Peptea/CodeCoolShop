f731655943f7b778ad0ab7fb580dbfa78b8328a4
mihaibuga <77620547+mihaibuga@users.noreply.github.com>
2021-11-17 10:19:59 +0200

    split app.jsx file into multiple components

---
+++ src/Codecool.CodecoolShop/wwwroot/js/Content.jsx
@@ -0,0 +1,11 @@
+﻿import OrdersList from '/js/OrdersList.jsx';
+
+const Content = () => {
+    return (
+        <div className="page-content">
+            <OrdersList url="/order/all" />
+        </div>
+    );
+}
+
+export default Content;
