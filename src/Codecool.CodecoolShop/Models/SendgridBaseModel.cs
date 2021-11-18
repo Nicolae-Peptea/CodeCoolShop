91000ab7756a4d595c441c166597f30b4cff006a
Nicolae <Nicolae@NICU>
2021-11-18 17:14:30 +0200

    change model to viewMOdel

---
+++ src/Codecool.CodecoolShop/Models/SendgridBaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codecool.CodecoolShop.ViewModels
+{
+    public class SendgridBaseModel
+    {
+        public string Email { get; set; }
+    }
+}
