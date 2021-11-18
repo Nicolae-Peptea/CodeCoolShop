5328f4e68229ac38a9281f8ec3bfb32e007e804c
Nicolae Peptea <nicolae.peptea@gmail.com>
2021-11-18 17:14:30 +0200

    change model to viewMOdel

---
+++ src/Codecool.CodecoolShop/Models/SendgridAccountConfirmationModel.cs
@@ -0,0 +1,21 @@
+﻿using Codecool.CodecoolShop.Helpers;
+using Microsoft.AspNetCore.Identity;
+
+namespace Codecool.CodecoolShop.ViewModels
+{
+    public class SendgridAccountConfirmationModel
+    {
+        public string FullName { get; private set; }
+
+        public string Link { get; private set; }
+
+        public string Email { get; private set; }
+
+        public SendgridAccountConfirmationModel(IdentityUser user, string link)
+        {
+            Email = user.Email;
+            FullName = UserNameHelper.ExtractUserNameFromEmail(user.Email);
+            Link = link;
+        }
+    }
+}
---
+++ src/Codecool.CodecoolShop/Models/SendgridOrderConfirmationModel.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Model;
+using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.ViewModels
+{
+    public class SendgridOrderConfirmationModel : SendgridBaseModel
+    {
+        public string FullName { get; private set; }
+
+        public string Address { get; private set; }
+
+        public string Country { get; private set; }
+
+        public string City { get; private set; }
+
+        public string ZipCode { get; private set; }
+
+        public string Total { get; private set; }
+
+        public List<ProductOrder> Items { get; private set; }
+
+        public SendgridOrderConfirmationModel(OrderViewDetails details, decimal orderTotal, List<ProductOrder> orderItems)
+        {
+            this.FullName = details.StripeBillingName;
+            this.Email = details.StripeEmail;
+            this.Address = details.StripeBillingAddressLine1;
+            this.Country = details.StripeBillingAddressCountry;
+            this.City = details.StripeBillingAddressCity;
+            this.ZipCode = details.StripeBillingAddressZip.ToString();
+            this.Total = orderTotal.ToString();
+            this.Items = orderItems;
+        }
+    }
+}
