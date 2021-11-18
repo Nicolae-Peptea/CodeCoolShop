5a71f2629ca2e3fceab48011bc0ebca221a9424e
mihaibuga <77620547+mihaibuga@users.noreply.github.com>
2021-11-18 12:00:30 +0200

    get order products on order expansion

--- src/Codecool.CodecoolShop/wwwroot/js/React/OrderProductList.jsx
+++ src/Codecool.CodecoolShop/wwwroot/js/React/OrderProductList.jsx
@@ -1,17 +1,14 @@
-﻿import useFetch from '/js/React/useFetch.jsx';
-
-const OrderProductList = ({ orderIdInUse }) => {
-    const [baseLink, setBaseLink] = React.useState("https://localhost:5001");
-    const { data: products } = useFetch(baseLink + "/order/getorderproducts?id=" + orderIdInUse);
-
+﻿const OrderProductList = ({ products }) => {
     return (
         <>
             <div className="order-details">
-                {[...Array(10)].map((product, i) =>
+                {products &&
+                    products.map((product, i) =>
                         <div className="order-details-row" key={i + 1}>
                             <div className="column">{i + 1}</div>
                             <div className="column">Product {i + 1}</div>
-                            <div className="column">$49.99</div>
+                            <div className="column">${product.PricePerProduct}</div>
+                            <div className="column">x {product.productQuantity}</div>
                         </div>
                 )}
             </div>
