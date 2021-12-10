fd5bfa36f19216a6076d0beb3015bea5c9ee9f09
Nicolae <nicolae.peptea@gmail.com>
2021-12-10 14:11:48 +0200

    bind order to user when registring

--- src/Codecool.CodecoolShop/Services/Interfaces/ICustomerService.cs
+++ src/Codecool.CodecoolShop/Services/Interfaces/ICustomerService.cs
@@ -11,5 +11,6 @@ namespace Codecool.CodecoolShop.Services.Interfaces
         void CreateCustomer(OrderViewDetailsModel order, HttpContext httpContext);
         string GetUserId(ClaimsPrincipal principal);
         int GetCustomerId(string userId);
+        void UpdateCustomerUserId(string email, string userId);
     }
 }
